37ac597a6b9cf0b928faf9e21fe8fbcf96c9ec9f
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2025-03-15 19:27:11 +0300

    Add content dispatcher that waits for incoming event and then sends it

---
+++ src/Domain/ContentDispatcher.cs
@@ -0,0 +1,11 @@
+﻿namespace Redplcs.PshenkaFeed.Domain;
+
+public class ContentDispatcher(IContentReceiver receiver, IContentSender sender)
+{
+	public async Task WaitForIncomingAndSendAsync(CancellationToken cancellationToken)
+	{
+		var content = await receiver.WaitForIncomingAsync(cancellationToken);
+
+		await sender.SendAsync(content, cancellationToken);
+	}
+}
