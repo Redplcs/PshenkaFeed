9d49124923400bf7780d69f310346c13173c3582
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2025-03-15 22:52:48 +0300

    Send attachments to specific topic

--- src/Infrastructure/Telegram/TelegramContentSender.cs
+++ src/Infrastructure/Telegram/TelegramContentSender.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Extensions.Options;
 using Redplcs.PshenkaFeed.Domain;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace Redplcs.PshenkaFeed.Infrastructure.Telegram;
 
 public sealed class TelegramContentSender(ITelegramBotClient api, IOptions<TelegramOptions> options) : IContentSender
 {
+	private readonly ChatId _chatId = options.Value.ChatId;
+	private readonly int _messageThreadId = options.Value.TopicId;
+
 	public Task SendAsync(Content content, CancellationToken cancellationToken)
 	{
 		return content.Type switch
@@ -18,11 +22,11 @@ public sealed class TelegramContentSender(ITelegramBotClient api, IOptions<Teleg
 
 	private async Task SendPhotoAsync(string url, CancellationToken cancellationToken)
 	{
-		await api.SendPhoto(options.Value.ChatId, url, cancellationToken: cancellationToken);
+		await api.SendPhoto(_chatId, url, messageThreadId: _messageThreadId, cancellationToken: cancellationToken);
 	}
 
 	private async Task SendVideoAsync(string url, CancellationToken cancellationToken)
 	{
-		await api.SendVideo(options.Value.ChatId, url, cancellationToken: cancellationToken);
+		await api.SendVideo(_chatId, url, messageThreadId: _messageThreadId, cancellationToken: cancellationToken);
 	}
 }
